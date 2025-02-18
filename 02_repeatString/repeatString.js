40747bbce71f3f6d8dc525711a14d1f72e36cbf1
Riowel <frampanarvaezespartero@gmail.com>
2025-02-18 21:57:35 +0100

    01-02 Solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+string=""
+if (times<0)
+{
+    string="ERROR"
+}
+    for (i=1; i<=times; i++){
+        string+=word;
+    }
+    return string;
 };
 
 // Do not edit below this line
